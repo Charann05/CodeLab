4279bd09b6dc9301c0e969faf7874c059a3ab2ab
Charan <charannpoojary@gmail.com>
2025-10-17 20:56:08 +0530

    Update evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -1,6 +1,8 @@
+//This program tells if the given number is Even or Odd.
 
 n = int(input("Enter a number : "))
 if(n%2==0):
     print("The number is Even")
 else:
+
     print("The numberr is Odd")
