0752de92239848009d13d8c33b8c29599831bc08
Charan <charannpoojary@gmail.com>
2025-10-22 19:38:51 +0530

    Added program file

---
+++ unioninter.py
@@ -0,0 +1,8 @@
+
+
+a = [6,4,1,1,1,2,]
+b = [1,1,1,2,2,3,3,3,]
+print(set(a)&set(b))
+print(set(a)|set(b))
+#print(*(set(a)&set(b)))        #If you don't want to print the square brackets.
+#print(*(set(a)|set(b)))
