e947648aa2b1cca473c8125bd72fad3d1865ba27
Charan <charannpoojary@gmail.com>
2025-10-22 20:30:42 +0530

    Update unioninter.py

--- unioninter.py
+++ unioninter.py
@@ -1,8 +1,9 @@
-
+# This program performs union and intersection operations on two arrays.
 
 a = [6,4,1,1,1,2,]
 b = [1,1,1,2,2,3,3,3,]
 print(set(a)&set(b))
 print(set(a)|set(b))
 #print(*(set(a)&set(b)))        #If you don't want to print the square brackets.
+
 #print(*(set(a)|set(b)))
