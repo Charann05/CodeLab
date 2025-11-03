03a789278165a3b3d186f2725651432e3fe4bc9a
Charan <charannpoojary@gmail.com>
2025-11-03 21:23:32 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,16 +1,17 @@
 #This program sorts the elements of the given array.
 
-n = int(input("Enter the number of elements:"))
+n = int(input("Enter the number of elements: "))
 a = []
 
 for i in range(n):
-    val = int(input(f"Enter the element {i+1}: "))
+    val = int(input(f"Enter element {i+1}: "))
     a.append(val)
 
 for i in range(n-1):
-    if(a[i]>a[i+1]):
-        temp = a[i]
-        a[i] = a[i+1]
-        a[i+1] = temp
+    for j in range(n-1-i):
+        if a[j] > a[j+1]:
+            temp = a[j]
+            a[j] = a[j+1]
+            a[j+1] = temp
 
-print("Sorted array:",a)
+print("Sorted array:", a)
