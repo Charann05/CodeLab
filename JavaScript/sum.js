9679ea988482b2aa6ecfdcf369bb36749cc79d28
Charan <charannpoojary@gmail.com>
2026-01-29 21:42:57 +0530

    Add program file

---
+++ JavaScript/sum.js
@@ -0,0 +1,11 @@
+// This program logs a greeting and calculates the sum of two numbers.
+
+console.log("Hello, World");
+
+function CalculateSum(num1, num2){
+
+    let sum = num1 + num2;
+    console.log("The sum is : ",sum);
+}
+
+CalculateSum(5,6);
