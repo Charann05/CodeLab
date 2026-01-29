cf7047fa52a19d35838eb3870695667a4e6cd967
Charan <charannpoojary@gmail.com>
2026-01-29 21:44:18 +0530

    Add program file

---
+++ JavaScript/city.js
@@ -0,0 +1,15 @@
+// This program performs basic array operations on an array of cities.
+
+let cities = ["New York", "Los Angeles", "Chicago", "Miami", "Houston"];
+console.log("Total number of cities : "+cities.length);
+
+console.log("Intial Cities are : "+cities);
+
+cities.push("San Fransisco");
+console.log("Cities after performing push operation : "+cities);
+
+cities.shift();
+console.log("Cities after performing push operation : "+cities);
+
+let index = cities.indexOf("Miami");
+console.log("The index of Miami is : ",index);
