af0cb9a10098c1c21f63d4e8d071369abfa0045a
Charan <charannpoojary@gmail.com>
2026-01-29 21:43:50 +0530

    Add program file

---
+++ JavaScript/sumFA.js
@@ -0,0 +1,11 @@
+// This program logs a greeting and calculates the sum of two numbers using an arrow function.
+
+console.log("Hello, World");
+
+const CalculateSum = (num1, num2) =>{
+
+    let sum = num1 + num2;
+    console.log("The sum is : ",sum);
+}
+
+CalculateSum(5,6);
